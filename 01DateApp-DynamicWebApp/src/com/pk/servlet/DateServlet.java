a2c05acd558c0f194771091bb021ee9871dd3f6e
Pankaj <pankajpalai405@gmail.com>
2021-03-13 12:12:43 +0530

    updated

--- 01DateApp-DynamicWebApp/src/com/pk/servlet/DateServlet.java
+++ 01DateApp-DynamicWebApp/src/com/pk/servlet/DateServlet.java
@@ -12,12 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 /**
  *  This is a simple web application just to display date and time, i made it easier for understanding
  * @author Pankaj Palai
+ * 
+ * url of the application: http://localhost:5050/01DateApp-DynamicWebApp/durl
+ * 
+ * note: here durl is the url-pattern configured by me in web.xml configuration file
  *
  */
 
 public class DateServlet extends HttpServlet{
 	public void service(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
-		System.out.println("DateServlet.service()");
 		Date d=null;
 		d=new Date();
 		//create printwriter
@@ -27,6 +30,9 @@ public class DateServlet extends HttpServlet{
 		//give instruction to browser to display given response as html document
 		res.setContentType("text/html");
 		pw.println("Date and Time :"+d);
+		
+		//it is optional , system.out.println() print message to serrvler console , during execution just look at the
+		//server console then you will easily understood
 		System.out.println(d);
 	}
 	
